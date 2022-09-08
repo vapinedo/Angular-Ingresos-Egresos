dc82f3e2017d77f97a295e0bdcf4c68ac0fde572
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-09-08 15:42:40 -0500

    [add] Crear nuestro primer usuario

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -10,7 +12,11 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 export class RegisterComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private fb: FormBuilder) { 
+  constructor(
+    private fb: FormBuilder,
+    private authSvc: AuthService,
+    private router: Router
+  ) { 
     this.form = fb.group({
       nombre: ["", Validators.required],
       email: ["", Validators.required],
@@ -26,9 +32,16 @@ export class RegisterComponent implements OnInit {
   }
 
   onRegister(): void {
-    console.log(this.form);
-    console.log(this.form.valid);
-    console.log(this.form.value);
+    if(this.form.invalid) { return; }
+    const { nombre, email, password } = this.form.value;
+    this.authSvc.crearUsuario(nombre, email, password)
+      .then(credenciales => {
+        console.log(credenciales);
+        this.router.navigateByUrl("/");
+      })
+      .catch(error => {
+        console.warn(error);
+      });
   }
 
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(public auth: AngularFireAuth) { }
+
+  crearUsuario(nombre: string, email: string, password: string): Promise<any> {
+    return this.auth.createUserWithEmailAndPassword(email, password);
+  }
+}
