b2f7b7225f03275c2572106a36fff62482c5b6be
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-09-08 17:48:17 -0500

    [add] Obtener informacion del usuario activo en Firebase

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,6 +9,14 @@ export class AuthService {
 
   constructor(public auth: AngularFireAuth) { }
 
+  initAuthListener() {
+    this.auth.authState.subscribe(firebaseUser => {
+      console.log(firebaseUser);
+      console.log(firebaseUser?.uid);
+      console.log(firebaseUser?.email);
+    });
+  }
+
   crearUsuario(nombre: string, email: string, password: string): Promise<any> {
     return this.auth.createUserWithEmailAndPassword(email, password);
   }
