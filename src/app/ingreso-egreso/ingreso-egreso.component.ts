ff822ae240c90477c649d24dc52a31061368f379
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-09-19 20:29:25 -0500

    [add] Optimizaciones de loading

--- src/app/ingreso-egreso/ingreso-egreso.component.ts
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { AppState } from '../app.reducer';
+import * as uiActions from '../shared/ui.actions';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { IngresoEgreso } from '../models/ingreso-egreso.model';
 import { FeedBackService } from '../services/feedback.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
@@ -10,23 +14,28 @@ import { IngresoEgresoService } from '../services/ingreso-egreso.service';
   styles: [
   ]
 })
-export class IngresoEgresoComponent implements OnInit {
+export class IngresoEgresoComponent implements OnInit, OnDestroy {
   form: FormGroup;
   isIngreso: boolean = true;
+  isLoadingSpinner: boolean = false;
+  subscriptions = new Subscription();
 
   constructor(
     private fb: FormBuilder,
     private feedBackSvc: FeedBackService,
+    private store: Store<AppState>,
     private ingresoEgresoSvc: IngresoEgresoService
   ) { 
     this.form = fb.group({
       descripcion: ["", Validators.required],
       monto: ["", Validators.required],
-
     });
   }
 
   ngOnInit(): void {
+    this.subscriptions = this.store.select("ui").subscribe(state => {
+      this.isLoadingSpinner = state.isLoading;
+    });
   }
 
   toggleTipoIngreso(): void {
@@ -35,13 +44,19 @@ export class IngresoEgresoComponent implements OnInit {
 
   async onSubmitForm() {
     if (this.form.invalid) { return; }
-
+    
+    this.store.dispatch(uiActions.isLoading());
     const { descripcion, monto } = this.form.value;
     const ingresoEgreso = new IngresoEgreso(monto, descripcion, this.isIngreso);
     const response = await this.ingresoEgresoSvc.create(ingresoEgreso);
+    this.store.dispatch(uiActions.stopLoading());
     (response == undefined) 
       ? this.feedBackSvc.success()
       : this.feedBackSvc.error();
   }
 
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+  
 }
