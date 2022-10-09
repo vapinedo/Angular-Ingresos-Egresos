8a2c90a68113e64e856a4c6f06389578c231d939
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-10-09 16:08:09 -0500

    [add] Dashboard - Estadistica basica

--- src/app/ingreso-egreso/estadistica/estadistica.component.ts
+++ src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -1,4 +1,7 @@
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
 import { Component, OnInit } from '@angular/core';
+import { IngresoEgreso } from 'src/app/models/ingreso-egreso.model';
 
 @Component({
   selector: 'app-estadistica',
@@ -8,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstadisticaComponent implements OnInit {
 
-  constructor() { }
+  egresos: number = 0;
+  ingresos: number = 0;
 
+  totalEgresos: number = 0;
+  totalIngresos: number = 0;
+
+  constructor(private store: Store<AppState>) { 
+  }
+  
   ngOnInit(): void {
+    this.store.select("ingresoEgresos")
+      .subscribe(({ items }) => this.generarEstadistica(items));
+  }
+  
+  generarEstadistica(items: IngresoEgreso[]) {
+    for(const item of items) {
+      if(item.tipo === "ingreso") {
+        this.totalIngresos += item.monto;
+        this.ingresos++;
+      } else {
+        this.totalEgresos += item.monto;
+        this.egresos++;
+      }
+    }
   }
 
 }
